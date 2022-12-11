3f6d80f0184be0db8bf8e03b1b113bafe2506221
Aditya KUmar <113716943+adityakumar002216@users.noreply.github.com>
2022-12-12 03:20:33 +0530

    Update alternate_array_printing.cpp

--- array/alternate_array_printing.cpp
+++ array/alternate_array_printing.cpp
@@ -1,23 +1,51 @@
-#include<iostream>
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
 using namespace std;
+
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+// ar[] is the array 
+// n is the number of elements in array.
+void print(int ar[], int n)
+{
+    
+    // code here
+    
+    
+    
+    
+    for(int k=0;k<n;k+=2)
+    {
+        
+    cout<<ar[k]<<" ";
+    
+    }
+
+}
+
+//{ Driver Code Starts.
 int main()
 {
-    int n,x=0;
-cout<<"enter the size of array";
-cin>>n;
-int arr[n];
-for(int i=0;i<n;i++){
-    cin>>arr[i];
-}
-cout<<"printing alternate element of the array \n";
-for(int i=0;i<n;i++){
-    cout<<arr[i];
-    i++;
-}
-
-
-
+   int t;
+   cin>>t;
+  while(t--)
+   {
+     int ar[100001]={0};
+     int n;
+     cin>>n;
+     for(int i=0;i<n;i++)
+      cin>>ar[i];
+      print(ar,n);
+      cout<<endl;
+     }
 
 return 0;
-
 }
+
+// } Driver Code Ends
